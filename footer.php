38604e9dc7f6d34cc8c88e6c96989e4b174dbbe7
Alisher Turubekov <develepor@mail.ru>
2024-11-18 00:51:48 +0500

    news site adap export

---
+++ footer.php
@@ -0,0 +1,6 @@
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adap-export</title>
+    <link rel="icon" href="favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <nav class="nav">
+        <div class="container">
+            <div class="nav__block">
+                <div class="nav__logo">
+                    <img src="img/logo.png" alt="Adap export">
+                </div>
+                <ul class="nav__menu">
+                    <li class="nav__item"><a href="">Главная</a></li>
+                    <li class="nav__item"><a href="">О компании</a></li>
+                    <li class="nav__item"><a href="">Продукция</a></li>
+                    <li class="nav__item"><a href="">Схема работы</a></li>
+                    <li class="nav__item"><a href="">Новости</a></li>
+                    <li class="nav__item"><a href="">Контакты</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="header">
+        <div class="container">
+            <h1 class="header__title title">ADAP-Export</h1>
+            <img src="img/elevator.png" alt="Трактор" id="tractor">
+            <hr class="header__hr">
+            <h2 class="header__subtitle subtitle">
+            Закупка и экспорт <br>
+            сельскохозяйственной продукции
+            </h2>
+            <a href="#" class="header__btn">О компании</a>
+        </div>
+    </header>
+    
